d01ea67c6101a258807f312a6e5d6be0f10658fd
Visalan <visalanoffl@gmail.com>
2024-07-21 19:06:08 +0530

    last

--- c.js
+++ c.js
@@ -1,5 +1,4 @@
 let ph=document.getElementById("num");
-
 function add5()
 {
     var hehe;
@@ -22,6 +21,48 @@ function mul8() {
 function div3() {
     var hehe;
     hehe = Number(ph.value);
-    hehe = parseInt(hehe /3);
+    hehe = hehe /3;
+    ph.value = hehe;
+}
+
+function showAdvancedButtons() {
+    var advancedButtons = document.getElementById('advanced-buttons');
+    advancedButtons.style.display = 'block';
+}
+
+function sqr() {
+    var hehe;
+    hehe = Number(ph.value);
+    hehe = Math.pow(hehe,2);
+    ph.value = hehe;
+}
+function sqrt() {
+    var hehe;
+    hehe = Number(ph.value);
+    hehe = Math.sqrt(hehe);
+    ph.value = hehe;
+}
+function sin() {
+    var hehe;
+    hehe = Number(ph.value);
+    hehe = Math.sin(hehe);
+    ph.value = hehe;
+}
+function log10() {
+    var hehe;
+    hehe = Number(ph.value);
+    hehe = Math.log10(hehe);
+    ph.value = hehe;
+}
+function floor() {
+    var hehe;
+    hehe = Number(ph.value);
+    hehe = Math.floor(hehe);
+    ph.value = hehe;
+}
+function ceil() {
+    var hehe;
+    hehe = Number(ph.value);
+    hehe = Math.ceil(hehe);
     ph.value = hehe;
 }
