aa9757ec093cf55575987cee4a3d590e7acadf42
Visalan <visalanoffl@gmail.com>
2024-07-21 13:52:26 +0530

    first

---
+++ a.html/c.js
@@ -0,0 +1,27 @@
+let ph=document.getElementById("num");
+
+function add5()
+{
+    var hehe;
+    hehe=Number(ph.value);
+    hehe=hehe+5;
+    ph.value=hehe;
+}
+function sub7() {
+    var hehe;
+    hehe = Number(ph.value);
+    hehe = hehe -7;
+    ph.value = hehe;
+}
+function mul8() {
+    var hehe;
+    hehe = Number(ph.value);
+    hehe = hehe * 8;
+    ph.value = hehe;
+}
+function div3() {
+    var hehe;
+    hehe = Number(ph.value);
+    hehe = parseInt(hehe /3);
+    ph.value = hehe;
+}
